6d85d5fc807cb2770c2e23f388b9f377a13a6f6c
lacrisy21 <lcristinagilp@gmail.com>
2020-06-11 21:37:22 +0000

    fml

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,13 +4,13 @@
   *
   * Return: Always 0 (Success)
   */
-int main (void)
+int main(void)
 
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("size of an int: %zu byte(s)\n", sizeof(int));
 	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("size of a long long: %zu byte(s)\n", sizeof(long long));
-	printf("size of a float: %zu byte(s)\n", sizeof (float));
+	printf("size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
