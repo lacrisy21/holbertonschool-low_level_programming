8403da8625bc0af427e4116b93d23d9af3b768d2
lacrisy21 <lcristinagilp@gmail.com>
2020-06-13 20:25:19 +0000

    marimar

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+   * main - Print numbers separeted with comas from 00 to 99
+    *
+     * Return: Always 0 (Success)
+      */
+int main(void)
+{
+	int num1, num2;
+
+	for (num2 = 0; num2 <= 9; num2++)
+	{
+		for (num1 = 0; num1 <= 9; num1++)
+		{
+		putchar(num2 + 48);
+		putchar(num1 + 48);
+		if (num2 != 9 || num1 != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
