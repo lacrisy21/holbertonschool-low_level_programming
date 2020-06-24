978f78a94fb5eb29e253b49b68b5fb10b9f626b1
lacrisy21 <lcristinagilp@gmail.com>
2020-06-24 03:43:06 +0000

    betty commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,13 +2,13 @@
 /**
  * swap_int - swap the value of int
  * @a: entry value int
- * @b: entry value int 
+ * @b: entry value int
  */
 void swap_int(int *a, int *b)
 {
 	int a1 = *b;
 	int b1 = *a;
- 
+
 	*a = a1;
 	*b = b1;
 }
