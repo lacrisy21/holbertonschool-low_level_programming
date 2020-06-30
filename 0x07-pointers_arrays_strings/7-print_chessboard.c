47fb8f96160eeb6feba5cf289e2fce80b40f98b2
lacrisy21 <lcristinagilp@gmail.com>
2020-06-30 03:15:56 +0000

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,14 +4,15 @@
  * @a:  array
  * Return: void
  */
-void print_chessboard(char (*a)[8])int row, col;
+void print_chessboard(char (*a)[8])
 {
 	int row, col;
+
 	for (row = 0; row < 8; row++)
 	{
 		for (col = 0; col < 8; col++)
 	{
-		_putchar (a[row][col]);
+		_putchar(a[row][col]);
 	}
 	_putchar('\n');
 	}
