1a1a1d47ed95a9666c3310ed44e556a2656131bd
lacrisy21 <lcristinagilp@gmail.com>
2020-10-07 23:36:03 +0000

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,10 @@
 * _pow_recursion - returns x raised to the power of y
 * @x: Base value
 * @y: Power value
-* Return: -1 if Y is negative, 1 if Y is zero, x if Y is 1. Power raised by the base value
+* Return: -1 if Y is negative
+* 1 if Y is zero
+* x if Y is 1
+* Power raised by the base value
 */
 int _pow_recursion(int x, int y)
 {
