06590e73dc4bffd5648a7cc8613488628824efe1
lacrisy21 <lcristinagilp@gmail.com>
2020-10-08 00:14:19 +0000

    root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * aux - auxiliar function
+ * @x: parameter
+ *@n: number
+ * Return: square
+ */
+int aux(int x, int n)
+{
+
+	if (n == x * x)
+		return (x);
+
+	if (n < x * x)
+		return (-1);
+
+	return (aux(n, x + 1));
+}
+
+/**
+ * _sqrt_recursion - square root of a number
+ * @n: number
+ * Return: sqaure root or -1 if it's not natural
+ */
+int _sqrt_recursion(int n)
+{
+	return (aux(1, n));
+}
+ 
