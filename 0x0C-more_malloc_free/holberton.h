ac012c0c8f5a1925519bf0f736655831165effba
lacrisy21 <lcristinagilp@gmail.com>
2020-10-15 23:47:26 +0000

    header

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,6 +1,7 @@
 #ifndef HOLBERTON_H
 # define HOLBERTON_H
 void *malloc_checked(unsigned int b);
+int sizes(char *a);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
