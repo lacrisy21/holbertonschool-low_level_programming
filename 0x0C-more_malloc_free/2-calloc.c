2df7064aa72e207150f7dea324fdf62df95c4dda
lacrisy21 <lcristinagilp@gmail.com>
2020-10-16 00:04:09 +0000

    call

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,29 +1,41 @@
 #include "holberton.h"
+#include <stdlib.h>
+
 /**
- * *_calloc - allocates memory for an array
- * @nmemb: array; !nmemb = nmemb == 0
- * @size: elements to arrange
- * Return: pointer
+* _calloc - allocates memory for an array
+* @nmemb: Number of caracter
+* @size: var
+* Return: Pointer
 */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *puntero;
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+		if (p == '\0')
+			return (NULL);
+
+	_memset(p, 0, nmemb * size);
+	return (p);
+}
+
+/**
+* _memset - Fill memory with a constant byte
+* @s: Area
+* @b: Costant byte
+* @n: Number of bytes
+* Return: Pointer in area s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
 	unsigned int i;
 
-	i = 0;
-
-	if (!nmemb || !size)
-		return (NULL);
-
-	puntero = malloc(size * nmemb);
-
-	if (!puntero)
-		return (NULL);
-
-	while (i < size * nmemb)
-	{
-		puntero[i] = 0;
-		i++;
-	}
-	return (puntero);
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
 }
