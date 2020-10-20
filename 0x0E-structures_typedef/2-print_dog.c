e867749864320657125109e2c1d49565290a2f74
lacrisy21 <lcristinagilp@gmail.com>
2020-10-20 02:38:29 +0000

    print

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,34 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - prtins dog struct
+ * @d: pointer pointing to the struct
+*/
+void print_dog(struct dog *d)
+{
+
+	if (!d)
+	{
+		return;
+	}
+
+	if ((*d).name != NULL)
+	{
+		printf("Name: %s\n", (*d).name);
+	}
+	else
+	{
+		printf("Name: (nil)\n");
+	}
+
+	printf("Age: %f\n", (*d).age);
+
+	if ((*d).owner != NULL)
+	{
+		printf("Owner: %s\n", (*d).owner);
+	}
+	else
+	{
+		printf("Owner: (nil)\n");
+	}
+}
