339c425b25c05f3e63228b1391a23fb646eccd6f
lacrisy21 <lcristinagilp@gmail.com>
2020-10-21 23:12:07 +0000

    print

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+* print_name - print name pointing to a function
+* @name: name of the person
+* @f: points to the function
+* Return: Nothing.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != '\0')
+	f(name);
+}
