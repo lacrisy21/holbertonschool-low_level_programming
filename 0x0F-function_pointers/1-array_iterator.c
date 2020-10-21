072f31f6d73c115649e0822a7209d879e101d8de
lacrisy21 <lcristinagilp@gmail.com>
2020-10-21 23:35:33 +0000

    itera

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,9 +12,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (action != '\0')
+	if (action != '\0' || array != '\0')
 	{
-		for (i = 0; i != size; i++)
+		for (i = 0; i <= size; i++)
 			action(array[i]);
 	}
 }
