0d153e9698d2705ea33fb7a9af24b41487f8f0ed
lacrisy21 <lcristinagilp@gmail.com>
2020-10-22 21:31:06 +0000

    calc1

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+* op_add-returns the sum
+* @a: Int to sum
+* @b: int to sum
+* Return: result of sum
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* op_sub- resta
+* @a: Int to substract
+* @b: int to substract
+* Return: result of substract
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+* op_mul - returns the mult
+* @a: Int to mult
+* @b: int to mult
+* Return: result of sum
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+* op_div - returns the div
+* @a: Int to div
+* @b: int to div
+* Return: result of sum
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+* op_mod-modulo
+* @a: Int to div
+* @b: int to module
+* Return: result of sum
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
