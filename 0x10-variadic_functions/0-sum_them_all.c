f052ead452f0f7d35566c1f60e8ffd76eb59c38f
lacrisy21 <lcristinagilp@gmail.com>
2020-10-25 13:09:56 +0000

    suma

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,6 +1,6 @@
 #include "variadic_functions.h"
 /**
- * sum_them_all - function that returns the sum of all its parameters.
+ * sum_them_all - returns the sum of all its parameters.
  * @n: The total size of the arguments.
  * Return: the sum of the parameters.
  */
